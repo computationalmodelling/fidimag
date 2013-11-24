cfabea6165aad63a1c8e904106c159324658daee
Weiwei Wang <ww1g11@soton.ac.uk>
2013-11-24 12:04:15 +0000

    update

--- lib/install.sh
+++ lib/install.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
 SUNDIALS=sundials-2.5.0
+FFTW=fftw-3.3.3
 
 #============================================================================
 #sundials
 #============================================================================
 if [ ! -e ${SUNDIALS}.tar.gz ]; then
-  wget http://ftp.mcs.anl.gov/pub/petsc/externalpackages/sundials-2.5.0.tar.gz
-  tar -xvzf ${SUNDIALS}.tar.gz
-  cd ${SUNDIALS}
-  ./configure
-  make
-  sudo make install
+  wget http://ftp.mcs.anl.gov/pub/petsc/externalpackages/${SUNDIALS}.tar.gz
 fi;
 
+if [ ! -e ${SUNDIALS} ]; then
+  tar -xvzf ${SUNDIALS}.tar.gz
+  cd ${SUNDIALS}
+  ./configure --enable-shared
+  make
+  sudo make install
+  cd ..
+fi;
 
+#============================================================================
+# fftw3
+#============================================================================
 
-#the same as fftw. will add later ....
+if [ ! -e ${FFTW}.tar.gz ]; then
+  wget http://www.fftw.org/${FFTW}.tar.gz
+fi;
+
+if [ ! -e ${FFTW} ]; then
+  tar -xvzf ${FFTW}.tar.gz
+  cd ${FFTW}
+  ./configure --enable-shared
+  make
+  sudo make install
+  cd ..
+fi;
--- pc/batch_task.py
+++ pc/batch_task.py
@@ -1,7 +1,7 @@
 import os
 import time
 import numpy as np
-from multiprocessing import Process,Queue, Lock
+from multiprocessing import Process, Queue, Lock
 
 lock=Lock()
 
