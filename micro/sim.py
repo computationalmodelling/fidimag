2639026f1d114ddd905179b92f658e5e5162d1ef
Weiwei Wang <ww1g11@soton.ac.uk>
2014-08-29 23:48:59 +0200

    add sim

---
+++ micro/sim.py
@@ -0,0 +1,21 @@
+from llg import LLG
+
+KNOWN_DRIVERS = {'llg':LLG}
+
+def Sim(*args, **kwargs):
+    
+    driver = 'llg'
+    
+    if kwargs.has_key('driver'):
+        driver = kwargs['driver']
+        kwargs.pop('driver')
+    
+    if driver not in KNOWN_DRIVERS:
+        raise NotImplementedError("""Driver '{}' is not implemented.
+                                  Valid choices: one of '{}'.""".format(driver, KNOWN_DRIVERS.keys()))
+    
+    return KNOWN_DRIVERS[driver](*args, **kwargs)
+
+
+if __name__=='__main__':
+    pass
