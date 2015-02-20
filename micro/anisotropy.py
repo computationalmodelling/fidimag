9647d839e4ee7630e8f8b02339a031233731b8aa
Mark Vousden <mark.vousden@soton.ac.uk>
2015-02-20 12:02:26 +0000

    Change name of import in anisotropy.py from pccp to fidimag.

--- micro/anisotropy.py
+++ micro/anisotropy.py
@@ -1,8 +1,8 @@
-import clib 
+import clib
 import numpy as np
 from pc.constant import mu_0
 from energy import Energy
-import pccp.util.helper as helper
+import fidimag.util.helper as helper
 
 class UniaxialAnisotropy(Energy):
     """
@@ -12,22 +12,22 @@ class UniaxialAnisotropy(Energy):
         self._Ku = Ku
         self.name = name
         self.axis = axis
-    
-    
+
+
     def setup(self, mesh, spin, Ms):
         super(UniaxialAnisotropy, self).setup(mesh, spin, Ms)
 
         self.Ms_inv_long = np.zeros(3*mesh.nxyz)
-        
+
         self.Ms_inv_long.shape = (3,-1)
         for i in range(mesh.nxyz):
             if self.Ms[i] == 0.0:
                 self.Ms_inv_long[:,i] = 0
             else:
                 self.Ms_inv_long[:,i] = 1.0/(mu_0*self.Ms[i])
-    
+
         self.Ms_inv_long.shape = (-1,)
-        
+
         self.Ku = np.zeros(3*self.nxyz, dtype=np.float)
         Ku_scalar = helper.init_scalar(self._Ku, self.mesh)
         self.Ku.shape = (3,-1)
@@ -41,12 +41,12 @@ class UniaxialAnisotropy(Energy):
                                 self.field,
                                 self.Ku,
                                 self.nxyz)
-                                
+
         return self.field*self.Ms_inv_long
-    
+
     def compute_energy(self):
         self.energy=clib.compute_anisotropy_energy(self.spin,
                                             self.Ku,
                                             self.nxyz)
-        
+
         return self.energy*self.mesh.cellsize
