c8f4bc7d2c495064a072d15235113fb1b217ac85
Weiwei Wang <ww1g11@soton.ac.uk>
2015-04-21 17:07:37 +0100

    try to add pins to neb

--- cp/neb/neb_clib.pyx
+++ cp/neb/neb_clib.pyx
@@ -6,7 +6,7 @@ np.import_array()
 
 cdef extern from "neb.h":
     void compute_tangents_c(double *ys, double *energy, double *tangents, int image_num, int nodes)
-    void compute_dm_dt_c(double *ys, double *heff, double *dm_dt, int image_num, int nodes) 
+    void compute_dm_dt_c(double *ys, double *heff, double *dm_dt, int *pins, int image_num, int nodes) 
 
 
 def compute_tangents(np.ndarray[double, ndim=2, mode="c"] ys,
@@ -20,7 +20,8 @@ def compute_tangents(np.ndarray[double, ndim=2, mode="c"] ys,
 def compute_dm_dt(np.ndarray[double, ndim=2, mode="c"] ys,
                             np.ndarray[double, ndim=2, mode="c"] heff,
                             np.ndarray[double, ndim=2, mode="c"] dmdt,
+                            np.ndarray[int, ndim=1, mode="c"] pins,
                             total_image_num,
                             nodes):
 
-    compute_dm_dt_c(&ys[0,0], &heff[0,0], &dmdt[0,0], total_image_num, nodes)
+    compute_dm_dt_c(&ys[0,0], &heff[0,0], &dmdt[0,0], &pins[0], total_image_num, nodes)
