7426c2cb8624ba93e9b7b58739b9a0cef979598b
Marc-Antonio Bisotti <mail@marc-antonio.de>
2015-04-27 17:04:02 +0100

    refactor install script, use function and install to "local" dir

--- bin/install.sh
+++ bin/install.sh
@@ -1,40 +1,42 @@
 #!/bin/bash
 
+# This script installs SUNDIALS and FFTW locally. It may need to environment
+# variables to work, like 'export CC=gcc' in ARCHER.
+
 SUNDIALS=sundials-2.5.0
 FFTW=fftw-3.3.3
-#Note: sometimes we need set variables to make it work, such as in ARCHER 'export CC=gcc' is necessary.
 
-LIBS_DIR=`pwd`
+set -e
 
-#============================================================================
-#sundials
-#============================================================================
-if [ ! -e ${SUNDIALS}.tar.gz ]; then
-  wget http://ftp.mcs.anl.gov/pub/petsc/externalpackages/${SUNDIALS}.tar.gz
-fi;
+# Create target directory if needed.
 
-if [ ! -e ${SUNDIALS} ]; then
-  tar -xvzf ${SUNDIALS}.tar.gz
-  cd ${SUNDIALS}
-  ./configure --enable-shared --prefix=${LIBS_DIR} --disable-lapack
-  make
-  make install
-  cd ..
-fi;
+HERE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+FIDIMAG_DIR="$(dirname "$HERE_DIR")"
+LIBS_DIR=${FIDIMAG_DIR}/local
+mkdir -p $LIBS_DIR
+cd ${LIBS_DIR}
+echo "Installing SUNDIALS and FFTW to "$LIBS_DIR"."
 
-#============================================================================
-# fftw3
-#============================================================================
+download_and_install() {
+    # $1 name of the package
+    # $2 URL where ${1}.tar.gz can be obtained
+    # $3 configure options
+    if [ ! -e ${1}.tar.gz ]; then
+        echo "Downloading "${1}"."
+        wget ${2}/${1}.tar.gz
+    fi;
 
-if [ ! -e ${FFTW}.tar.gz ]; then
-  wget http://www.fftw.org/${FFTW}.tar.gz
-fi;
+    if [ ! -e ${1} ]; then
+        tar -xvzf ${1}.tar.gz
+        cd ${1}
+        ./configure --enable-shared --prefix=${LIBS_DIR} $3
+        make
+        make install
+        cd ${LIBS_DIR}
+    fi;
+}
 
-if [ ! -e ${FFTW} ]; then
-  tar -xvzf ${FFTW}.tar.gz
-  cd ${FFTW}
-  ./configure --enable-shared --prefix=${LIBS_DIR} --enable-openmp
-  make
-  make install
-  cd ..
-fi;
+download_and_install ${SUNDIALS} http://ftp.mcs.anl.gov/pub/petsc/externalpackages --disable-lapack
+download_and_install ${FFTW} http://www.fftw.org --enable-openmp
+
+echo "Installation succesful."
