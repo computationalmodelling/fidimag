fe5681bf004373ad9f52b54856315d30bea90535
Marc-Antonio Bisotti <mail@marc-antonio.de>
2015-04-27 18:02:21 +0100

    simplify and shorten setup.py, now builds into ./fidimag/extensions instead of cwd
    also, PEP8

--- Makefile
+++ Makefile
@@ -1,17 +1,26 @@
-.PHONY: build
-
-# build cython files
-
 PROJECT_DIR = $(abspath .)
+EXTENSIONS_DIR = ${PROJECT_DIR}/fidimag/extensions
 
-build:
-	python setup.py build_ext --inplace
+#####################
+# Cython Extensions #
+#####################
+
+extensions-directory:
+	mkdir -p ${EXTENSIONS_DIR}
+
+build: extensions-directory
+	python setup.py build_ext --build-lib ${EXTENSIONS_DIR}
+	rm -rf ${PROJECT_DIR}/build
+
+clean:
+	rm -rf ${EXTENSIONS_DIR}
+
+#########
+# Tests #
+#########
 
 create-dirs:
 	mkdir -p test-reports/junit
-
-clean:
-	rm *.so
 
 test: create-dirs
 	py.test -v --junitxml=$(PROJECT_DIR)/test-reports/junit/test-pytest.xml
@@ -25,7 +34,10 @@ test-basic:
 test-ipynb: create-dirs
 	cd doc/ipynb && py.test . -v --ipynb --sanitize-with sanitize_file --junitxml=$(PROJECT_DIR)/test-reports/junit/test-ipynb-pytest.xml
 
-# Building documentation
+#################
+# Documentation #
+#################
+
 doc: doc-html doc-latexpdf doc-singlehtml
 
 doc-clean:
@@ -34,3 +46,5 @@ doc-clean:
 doc-%:
 	@echo $*
 	make -C doc $*
+
+.PHONY: extensions-directory build clean create-dirs test test-micro test-basic test-ipynb doc doc-clean doc-html doc-latexpdf doc-singlehtml
--- setup.py
+++ setup.py
@@ -1,127 +1,97 @@
+"""
+Use this script with `python setup.py build_ext --inplace`
+or by using the Makefile.
+
+"""
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 import numpy
-
-#python setup.py build_ext --inplace
-
 import fnmatch
 import os
 import glob
 
 os.environ["CC"] = "gcc"
 
-#print __file__
-#print os.getcwd()
-realpath=os.path.realpath(__file__)
-pccp_path=os.path.split(realpath)[0]
+MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
+SRC_DIR = os.path.join(MODULE_DIR, "fidimag")
+CP_DIR = os.path.join(SRC_DIR, "cp")
+SUNDIALS_DIR = os.path.join(CP_DIR, "sundials")
+BARYAKHTAR_DIR = os.path.join(SRC_DIR, "baryakhtar", "cython")
+MICRO_DIR = os.path.join(SRC_DIR, "micro", "cython")
+NEB_DIR = os.path.join(CP_DIR, "neb")
+LOCAL_DIR = os.path.join(MODULE_DIR, "local")
+INCLUDE_DIR = os.path.join(LOCAL_DIR, "include")
+LIB_DIR = os.path.join(LOCAL_DIR, "lib")
 
-cp_path=os.path.join(pccp_path,'cp')
-os.chdir(pccp_path)
+
+def glob_cfiles(path, excludes):
+    cfiles = []
+    for cfile in glob.glob(os.path.join(path, "*.c")):
+        if not cfile.endswith(tuple(excludes)):
+            cfiles.append(cfile)
+    return cfiles
 
 sources = []
-sources.append(os.path.join(cp_path,'clib.pyx'))
+sources.append(os.path.join(CP_DIR, 'clib.pyx'))
+sources += glob_cfiles(CP_DIR, excludes=["clib.c"])
 
-
-cfiles = glob.glob(os.path.join(cp_path,'*.c'))
-for cf in cfiles:
-    if not cf.endswith("clib.c"):
-        sources.append(cf)
-
-print 'sources',sources
-
-
-sundials_path = os.path.join(cp_path,'sundials')
 cvode_sources = []
-cvode_sources.append(os.path.join(sundials_path,'cvode.pyx'))
-cfiles = glob.glob(os.path.join(sundials_path,'*.c'))
-for cf in cfiles:
-    if not cf.endswith("cvode.c"):
-        cvode_sources.append(cf)
-
-
-baryakhtar_path=os.path.join(pccp_path,'baryakhtar')
-baryakhtar_path = os.path.join(baryakhtar_path,'cython')
+cvode_sources.append(os.path.join(SUNDIALS_DIR, 'cvode.pyx'))
+cvode_sources += glob_cfiles(SUNDIALS_DIR, excludes=["cvode.c"])
 
 baryakhtar_sources = []
-baryakhtar_sources.append(os.path.join(baryakhtar_path,'baryakhtar_clib.pyx'))
-cfiles = glob.glob(os.path.join(baryakhtar_path,'*.c'))
-for cf in cfiles:
-    if not cf.endswith("baryakhtar_clib.c"):
-        baryakhtar_sources.append(cf)
+baryakhtar_sources.append(os.path.join(BARYAKHTAR_DIR, 'baryakhtar_clib.pyx'))
+baryakhtar_sources += glob_cfiles(BARYAKHTAR_DIR, excludes=["baryakhtar_clib.c"])
 
-
-micro_path=os.path.join(pccp_path,'micro')
-micro_path = os.path.join(micro_path,'cython')
 micro_sources = []
-micro_sources.append(os.path.join(micro_path,'micro_clib.pyx'))
-cfiles = glob.glob(os.path.join(micro_path,'*.c'))
-for cf in cfiles:
-    if not cf.endswith("micro_clib.c"):
-        micro_sources.append(cf)
+micro_sources.append(os.path.join(MICRO_DIR, 'micro_clib.pyx'))
+micro_sources += glob_cfiles(MICRO_DIR, excludes=["micro_clib.c"])
 
-
-neb_path = os.path.join(cp_path,'neb')
 neb_sources = []
-neb_sources.append(os.path.join(neb_path,'neb_clib.pyx'))
-cfiles = glob.glob(os.path.join(neb_path,'*.c'))
-for cf in cfiles:
-    if not cf.endswith("neb_clib.c"):
-        neb_sources.append(cf)
-
-
-libs_path = os.path.join(pccp_path,'libs')
-include_path = os.path.join(libs_path,'include')
-lib_path = os.path.join(libs_path,'lib')
-
+neb_sources.append(os.path.join(NEB_DIR, 'neb_clib.pyx'))
+neb_sources += glob_cfiles(NEB_DIR, excludes=["neb_clib.c"])
 
 ext_modules = [
     Extension("clib",
-              sources = sources,
-              include_dirs = [numpy.get_include(),include_path],
-              libraries=['m','fftw3_omp','fftw3','sundials_cvodes','sundials_nvecserial'],
+              sources=sources,
+              include_dirs=[numpy.get_include(), INCLUDE_DIR],
+              libraries=['m', 'fftw3_omp', 'fftw3', 'sundials_cvodes', 'sundials_nvecserial'],
               extra_compile_args=["-fopenmp", '-std=c99'],
-              extra_link_args=['-L%s'%lib_path,'-fopenmp'],
-        ),
+              extra_link_args=['-L%s' % LIB_DIR, '-fopenmp'],
+              ),
     Extension("cvode",
-              sources = cvode_sources,
-              include_dirs = [numpy.get_include(),include_path],
-              libraries=['m','fftw3','sundials_cvodes','sundials_nvecserial'],
+              sources=cvode_sources,
+              include_dirs=[numpy.get_include(), INCLUDE_DIR],
+              libraries=['m', 'fftw3', 'sundials_cvodes', 'sundials_nvecserial'],
               extra_compile_args=["-fopenmp", '-std=c99'],
-              extra_link_args=['-L%s'%lib_path,'-fopenmp'],
-              #extra_link_args=["-g"],
-        ),
-               
+              extra_link_args=['-L%s' % LIB_DIR, '-fopenmp'],
+              ),
     Extension("baryakhtar_clib",
-              sources = baryakhtar_sources,
-              include_dirs = [numpy.get_include(),include_path],
-              libraries=['m','fftw3','sundials_cvodes','sundials_nvecserial'],
+              sources=baryakhtar_sources,
+              include_dirs=[numpy.get_include(), INCLUDE_DIR],
+              libraries=['m', 'fftw3', 'sundials_cvodes', 'sundials_nvecserial'],
               extra_compile_args=["-fopenmp", '-std=c99'],
-              extra_link_args=['-L%s'%lib_path,'-fopenmp'],
-              #extra_link_args=["-g"],
-        ),
-    
+              extra_link_args=['-L%s' % LIB_DIR, '-fopenmp'],
+              ),
     Extension("micro_clib",
-              sources = micro_sources,
-              include_dirs = [numpy.get_include(),include_path],
-              libraries=['m','fftw3','sundials_cvodes','sundials_nvecserial'],
+              sources=micro_sources,
+              include_dirs=[numpy.get_include(), INCLUDE_DIR],
+              libraries=['m', 'fftw3', 'sundials_cvodes', 'sundials_nvecserial'],
               extra_compile_args=["-fopenmp", '-std=c99'],
-              extra_link_args=['-L%s'%lib_path,'-fopenmp'],
-              #extra_link_args=["-g"],
-       ),
+              extra_link_args=['-L%s' % LIB_DIR, '-fopenmp'],
+              ),
     Extension("neb_clib",
-              sources = neb_sources,
-              include_dirs = [numpy.get_include(),include_path],
-              libraries=['m','fftw3_omp','fftw3','sundials_cvodes','sundials_nvecserial'],
+              sources=neb_sources,
+              include_dirs=[numpy.get_include(), INCLUDE_DIR],
+              libraries=['m', 'fftw3_omp', 'fftw3', 'sundials_cvodes', 'sundials_nvecserial'],
               extra_compile_args=["-fopenmp", '-std=c99'],
-              extra_link_args=['-L%s'%lib_path,'-fopenmp'],
-        ),
-
-    ]
-    
+              extra_link_args=['-L%s' % LIB_DIR, '-fopenmp'],
+              ),
+]
 
 setup(
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = ext_modules
+    cmdclass={'build_ext': build_ext},
+    ext_modules=ext_modules
 )
