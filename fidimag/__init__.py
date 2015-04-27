778d96ce655631c8c3272f0bc5c950f582ad3249
Marc-Antonio Bisotti <mail@marc-antonio.de>
2015-04-27 20:25:15 +0100

    build cython extensions automatically if needed, no need to `make build` anymore

--- fidimag/__init__.py
+++ fidimag/__init__.py
@@ -1,4 +1,14 @@
+try:
+    from . import extensions
+except ImportError as e:
+    import os
+    cwd = os.getcwd()
+    FIDIMAG_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    os.chdir(FIDIMAG_DIR)
+    print "Could not find fidimag extensions. Trying to build them now..."
+    os.system("make build")
+    print "Building extensions done."
+    os.chdir(cwd)
 from . import util
 from . import pc
-from . import extensions
 from . import micro
