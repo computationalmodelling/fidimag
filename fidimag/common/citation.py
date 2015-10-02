76ea4a8b267338f7a6e0c56ee2fd445937293542
Hans Fangohr Lubuntu 14.04 virt MBA <fangohr@soton.ac.uk>
2015-10-02 17:11:03 +0100

    Adding citation() command

---
+++ fidimag/common/citation.py
@@ -0,0 +1,6 @@
+def citation():
+    """Provide citation string."""
+    s = """Fidimag - FInite DIfference microMAGnetic simulator. 
+https://github.com/fangohr/fidimag
+(C) University of Southampton, 2012 - 2015"""
+    return s
