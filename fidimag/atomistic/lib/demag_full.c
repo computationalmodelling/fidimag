b09df84bd20ce1f26678a550f38dc24e53851018
davidcorteso <david.cortes.o@gmail.com>
2015-12-03 13:13:05 +0000

    Documented C library for full demag calculation. Added test for hexagonal lattice demag field.

--- fidimag/atomistic/lib/demag_full.c
+++ fidimag/atomistic/lib/demag_full.c
@@ -5,41 +5,96 @@
 void demag_full(double *spin, double *field, double *energy, double *coords,
                 double *mu_s, double *mu_s_scale, int n) {
 
-    /* Full calculation of Demag */
+    /* Full calculation of the Demagnetising field for atomistic systems
+     * The main idea is to iterate through every lattice site, and sum
+     * the dipolar contribution of the whole system. This is not the most
+     * effective aproach since it needs N^2 calculations, but it is reliable
+     * to compare against other approximations or techniques
+     *
+     * Thus, for the i-th spin, the field is calculated as:
+     *
+     *                                   ^      ^         ^        ^
+	 *   ->	     mu0 mu_s        __    3 r_ij ( m_j \cdot r_ij ) - m_j
+     *   H_i =   --------    +  \	   -------------------------------
+     *             4 pi         /__              r_ij ^ 3
+     *
+     *                        i != j
+     *
+     * where the numerator has unit vectors.
+     * r_ij is a vector from r_i to r_j , i.e.  r_ij = r_j - r_i
+     *
+     * The prefactor for every lattice site, is stored in the mu_s_scale
+     * array, since mu_s will depend on the site (different or no material)
+     *
+     * The spin and field are vector fields, thus they have 3 * n entries,
+     * where n is the number of lattice sites of the system (including the
+     * ones with mu_s = 0)
+     * The coords array also has 3 * n entries since it has the coordinates
+     * for every point, in the order:   x0 y0 z0 x1 y1 z1 x2 ...
+     *
+     * mu_s has the magnetic moments magnitudes and it is used for computing
+     * the energy density of the i-th spin as:
+     *
+	 *   	       mu_s       __  ^         ->
+     *   E_i =  -   --    +  \	  m_i \cdot H_i
+     *               2       /__
+     *
+     *                     i=x,y,z
+     *
+     * the 1/2 factor is for the double counting.
+     *
+     */
 
+    /* we start iterating through every lattice site */
 	#pragma omp parallel
 	for (int i = 0; i < n; i++) {
+        /* rij for the distance vector and rij_n for the normalised
+         * version of the vector. rij_mag is the magnitude */
         double* rij = malloc(3 * sizeof(double));
         double* rij_n = malloc(3 * sizeof(double));
-        double mrij = 0;
         double rij_mag;
+        /* This is for the dot product of m and rij */
+        double mrij = 0;
+
+        /* Reset the field values */
         for (int k=0; k < 3; k++) field[3 * i + k] = 0;
 
+        /* Now we iterate through every spin of the system excluding
+         * the point where we are right now */
         for (int j = 0; j < n; j++) {
 
+            /* Avoid sites with mu_s = 0 */
             if(j != i && mu_s_scale[j] != 0.){
-                
+
+                /* Compute the distance from r_i to r_j and normalise */
                 for(int k = 0; k < 3; k++) {
                     rij[k] = coords[3 * j + k] - coords[3 * i + k];
                 }
 
-                rij_mag = sqrt(rij[0] * rij[0] + 
-                               rij[1] * rij[1] + 
+                rij_mag = sqrt(rij[0] * rij[0] +
+                               rij[1] * rij[1] +
                                rij[2] * rij[2]);
 
                 for(int k = 0; k < 3; k++) rij_n[k] = rij[k] / rij_mag;
 
+                /* dot product of m_j and r_ij
+                 * Remember that m has the structure: mx0 my0 mz0 mx1 my1 ...
+                 */
                 mrij = spin[3 * j] * rij[0] + spin[3 * j + 1] * rij[1]
                        + spin[3 * j + 2] * rij[2] ;
 
+                /* Now add the contribution of the j-th spin */
                 for(int k = 0; k < 3; k++){
                     field[3 * i + k] += (3 * rij_n[k] * mrij - spin[3 * j + k])
                                          / (rij_mag * rij_mag * rij_mag) ;
                 }
             }
         }
+
+        /* Now we scale the total contribution to the i-th spin */
         for(int k = 0; k < 3; k++) field[3 * i + k] *= mu_s_scale[i];
 
+        /* And compute the energy avoiding double counting */
         energy[i] = -0.5 * mu_s[i] * (field[3 * i]     * spin[3 * i]     +
                                       field[3 * i + 1] * spin[3 * i + 1] +
                                       field[3 * i + 2] * spin[3 * i + 2]);
--- tests/test_demag_libraries.py
+++ tests/test_demag_libraries.py
@@ -1,17 +1,28 @@
 """
-Testing a brute force calculation of the demagnetising field
-against the default approach, based on a Fourier transformation.
+Testing of the different functions to compute the demag
+field for atomistic simulations (in micromagnetic simulations
+we only use cuboid meshes, so we only have the FFT approach
+for the dipolar field)
 """
 
 from fidimag.atomistic import Sim
 from fidimag.common import CuboidMesh
-from fidimag.atomistic import Demag, DemagFull
+from fidimag.atomistic.hexagonal_mesh import HexagonalMesh
+from fidimag.atomistic import Demag, DemagFull, DemagHexagonal
 from fidimag.atomistic import Constant
 
 const = Constant()
 
 
 def test_cuboid_demags():
+    """
+    Test a brute force calculation of the demagnetising field, called
+    DemagFull, based on the sum of the dipolar contributions of the whole
+    system for every lattice site, against the default FFT approach for the
+    demag field. We compute the energies scaled in meV.
+    This test is performed in a cuboid mesh to assure that the DemagFull
+    library is calculating the same than the default demag function
+    """
 
     mesh = CuboidMesh(0.27, 0.27, 0.27, 4, 4, 1, unit_length=1e-9)
     mu_s = 2 * const.mu_B
@@ -42,5 +53,46 @@ def test_cuboid_demags():
     # We compare both energies scaled in meV
     assert (demag_full_energy - demag_fft_energy) < 1e-10
 
+
+def test_hexagonal_demags():
+    """
+    Comparison of the FFT approach for hexagonal meshes, named
+    DemagHexagonal, where it is used a system with the double number
+    of nodes along the x direction (i.e. a mesh with twice the number
+    of nodes of the original mesh), against the full calculation
+    of the Demag field
+    """
+
+    mesh = HexagonalMesh(0.27 * 0.5, 4, 4, unit_length=1e-9)
+    mu_s = 2 * const.mu_B
+
+    sim = Sim(mesh)
+    sim.mu_s = mu_s
+
+    sim.set_m((0, 0, 1))
+    # Brute force demag calculation
+    sim.add(DemagFull())
+
+    sim.get_interaction('demag_full').compute_field()
+    # print sim.get_interaction('demag_full').field
+    demag_full_energy = sim.compute_energy() / const.meV
+
+    # Demag using the FFT approach and a larger mesh
+    sim2 = Sim(mesh)
+    sim2.mu_s = mu_s
+
+    sim2.set_m((0, 0, 1))
+
+    sim2.add(DemagHexagonal())
+    sim2.get_interaction('demag_hex').compute_field()
+    sim2.compute_energy()
+
+    demag_2fft_energy = sim2.compute_energy() / const.meV
+
+    # We compare both energies scaled in meV
+    assert (demag_full_energy - demag_2fft_energy) < 1e-10
+
+
 if __name__ == '__main__':
     test_cuboid_demags()
+    test_hexagonal_demags()
