19774fa219e44989067c4b76d2674539acbb4549
logicabrity <mail@marc-antonio.de>
2016-02-19 10:35:38 +0000

    cosmetics

--- fidimag/common/sundials/cvode.pyx
+++ fidimag/common/sundials/cvode.pyx
@@ -1,96 +1,78 @@
+import numpy as np
+cimport numpy as np  # import special compile-time information about numpy
+np.import_array()  # don't remove or you'll segfault
 from libc.string cimport memcpy
 
-# Import the Python-level symbols of numpy
-import numpy as np
-
-# Import the C-level symbols of numpy
-cimport numpy as np
-
-# Numpy must be initialized. When using numpy from C or Cython you must
-# _always_ do that, or you will have segfaults
-np.import_array()
 
 cdef extern from "../../atomistic/lib/clib.h":
     void normalise(double *m, int nxyz)
+
 
 cdef struct cv_userdata:
     void *rhs_fun
     void *y
     void *dm_dt
     void *jvn_fun
-    void *v #is the vector by which the Jacobian must be multiplied.
-    void *jv #the value of Jacobian times v.
+    void *v  # is the vector by which the Jacobian must be multiplied
+    void *jv  # the value of Jacobian times v
 
-cdef inline  copy_arr2nv(np.ndarray[realtype, ndim=1, mode='c'] np_x, N_Vector v):
+
+cdef inline copy_arr2nv(np.ndarray[realtype, ndim=1, mode='c'] np_x, N_Vector v):
     cdef long int n = (<N_VectorContent_Serial>v.content).length
     cdef void* data_ptr=<void *>np_x.data
     memcpy((<N_VectorContent_Serial>v.content).data, data_ptr, n*sizeof(double))
-
     return 0
+
 
 cdef inline copy_nv2arr(N_Vector v, np.ndarray[realtype, ndim=1, mode='c'] np_x):
     cdef long int n = (<N_VectorContent_Serial>v.content).length
     cdef double* v_data = (<N_VectorContent_Serial>v.content).data
-
     memcpy(np_x.data, v_data, n*sizeof(realtype))
     return 0
 
+
 cdef int cv_rhs(realtype t, N_Vector yv, N_Vector yvdot, void* user_data) except -1:
-
     cdef cv_userdata *ud = <cv_userdata *>user_data
-
-    #cdef int size = (<N_VectorContent_Serial>yvdot.content).length
-    #cdef double *y = (<N_VectorContent_Serial>yv.content).data
-    #it's not suitable to call the normalise function here.
-    #normalise(&y[0], size/3)
-
     cdef np.ndarray[realtype, ndim=1, mode='c'] y_arr= <np.ndarray[realtype, ndim=1, mode='c']>ud.y
     cdef np.ndarray ydot_arr= <np.ndarray[realtype, ndim=1, mode='c']>ud.dm_dt
 
-    copy_nv2arr(yv,y_arr)
-    (<object>ud.rhs_fun)(t,y_arr,ydot_arr)
-
-    copy_arr2nv(ydot_arr,yvdot)
-
+    copy_nv2arr(yv, y_arr)
+    (<object>ud.rhs_fun)(t, y_arr, ydot_arr)
+    copy_arr2nv(ydot_arr, yvdot)
     return 0
 
+
 cdef int cv_jtimes(N_Vector v, N_Vector Jv, realtype t, N_Vector y, N_Vector fy, void *user_data, N_Vector tmp) except -1:
-
     cdef cv_userdata *ud = <cv_userdata *>user_data
-
     cdef np.ndarray[realtype, ndim=1, mode='c'] m_arr = <np.ndarray[realtype, ndim=1, mode='c']>ud.y
     cdef np.ndarray[realtype, ndim=1, mode='c'] mp_arr = <np.ndarray[realtype, ndim=1, mode='c']>ud.v
     cdef np.ndarray[realtype, ndim=1, mode='c'] ydot_arr = <np.ndarray[realtype, ndim=1, mode='c']>ud.dm_dt
     cdef np.ndarray[realtype, ndim=1, mode='c'] jv_arr = <np.ndarray[realtype, ndim=1, mode='c']>ud.jv
 
-    copy_nv2arr(y,m_arr)
-    copy_nv2arr(v,mp_arr)
-    copy_nv2arr(fy,ydot_arr)
-
+    copy_nv2arr(y, m_arr)
+    copy_nv2arr(v, mp_arr)
+    copy_nv2arr(fy, ydot_arr)
     (<object>ud.jvn_fun)(mp_arr, jv_arr, t, m_arr, ydot_arr)
-
     copy_arr2nv(jv_arr, Jv)
-
-    return 0
-
-
-cdef int psetup(realtype t, N_Vector y, N_Vector fy,
-        booleantype jok, booleantype *jcurPtr, realtype gamma,
-        void *user_data, N_Vector tmp1, N_Vector tmp2, N_Vector tmp3):
-    if not jok:
-        self.y[:] = y[:]
     return 0
 
 
 cdef int psolve(realtype t, N_Vector y, N_Vector fy,
-        N_Vector r, N_Vector z, realtype gamma, realtype delta, int lr,
-        void *user_data, N_Vector tmp):
+                N_Vector r, N_Vector z, realtype gamma, realtype delta, int lr,
+                void *user_data, N_Vector tmp):
     z[:] = r
     return 0
 
 
-cdef class CvodeSolver(object):
+cpdef int psetup(self, realtype t, N_Vector y, N_Vector fy,
+                 booleantype jok, booleantype *jcurPtr, realtype gamma,
+                 void *user_data, N_Vector tmp1, N_Vector tmp2, N_Vector tmp3):
+    if not jok:
+        self.y[:] = y
+    return 0
 
+
+cdef class CvodeSolver(object):
     cdef public double t
     cdef public np.ndarray y
     cdef double rtol, atol
@@ -99,20 +81,17 @@ cdef class CvodeSolver(object):
     cdef np.ndarray mp
     cdef np.ndarray Jmp
     cdef N_Vector u_y
-
     cdef void *cvode_mem
     cdef void *rhs_fun
     cdef void *jvn_fun
     cdef callback_fun
     cdef jtimes_fun
     cdef cv_userdata user_data
-
-    cdef long int nsteps,nfevals,njevals
+    cdef long int nsteps, nfevals, njevals
     cdef int max_num_steps
     cdef int has_jtimes
 
     def __cinit__(self, spin, callback_fun, jtimes_fun=None, rtol=1e-8, atol=1e-8):
-
         self.t = 0
         self.spin = spin
         self.dm_dt = np.copy(spin)
@@ -144,30 +123,27 @@ cdef class CvodeSolver(object):
         copy_arr2nv(spin, self.u_y)
         CVodeReInit(self.cvode_mem, t, self.u_y)
 
-    # The flag_m argument indicates if the magnetisation was already loaded
-    # since, when using CVOdeInit, new memory is allocated and this causes a
-    # memory issue when reloading the magnetisation (spin) multiple times Thus,
-    # in the llg classes, when calling this function, we initiate the flag as
-    # zero and then change it to 1, in order to use this same memory when
-    # reloading the integrator after setting the initial magnetisation
-    def set_initial_value(self, np.ndarray[double, ndim=1, mode="c"] spin,
-                          t, flag_m):
+    def set_initial_value(self, np.ndarray[double, ndim=1, mode="c"] spin, t, flag_m):
         self.t = t
         self.y[:] = spin[:]
 
-        cdef np.ndarray[double, ndim=1, mode="c"] y=self.y
-        self.u_y = N_VMake_Serial(y.size,&y[0])
+        cdef np.ndarray[double, ndim=1, mode="c"] y = self.y
+        self.u_y = N_VMake_Serial(y.size, &y[0])
 
+        # We make sure that new memory for the magnetisation data is only allocated
+        # once, by using the `flag_m` variable to signify allocation has already
+        # taken place. If `flag_m` is 1, we use CvodeReInit instead of CvodeInit,
+        # which reuses existing memory.
         if not flag_m:
             flag = CVodeInit(self.cvode_mem, <CVRhsFn>self.rhs_fun, t, self.u_y)
+            self.check_flag(flag, "CVodeInit")
         else:
             flag = CVodeReInit(self.cvode_mem, t, self.u_y)
-        self.check_flag(flag,"CVodeInit")
 
-        if self.has_jtimes:
+        if self.has_jtimes:  # we will use preconditioning with the Jacobian
             # CVSpgmr(cvode_mem, pretype, maxl) p. 27 of CVODE 2.7 manual
             flag = CVSpgmr(self.cvode_mem, PREC_LEFT, 300);
-            # p. 37 CVODE 2.7 manual
+            # functions below in p. 37 CVODE 2.7 manual
             flag = CVSpilsSetJacTimesVecFn(self.cvode_mem, <CVSpilsJacTimesVecFn>self.jvn_fun)
             flag = CVSpilsSetPreconditioner(self.cvode_mem, <CVSpilsPrecSetupFn>psetup, <CVSpilsPrecSolveFn>psolve)
         else:
@@ -200,16 +176,14 @@ cdef class CvodeSolver(object):
         self.t = tret  # FIXME: or should this be tf?
         return 0
 
-
     def check_flag(self, flag, fun_name):
-        if flag<0:
+        if flag < 0:
             raise Exception("Run %s failed!"%fun_name)
 
     def stat(self):
         CVodeGetNumSteps(self.cvode_mem, &self.nsteps);
         CVodeGetNumRhsEvals(self.cvode_mem, &self.nfevals);
         CVSpilsGetNumJtimesEvals(self.cvode_mem, &self.njevals)
-
         return self.nsteps, self.nfevals, self.njevals
 
     def get_current_step(self):
@@ -219,9 +193,9 @@ cdef class CvodeSolver(object):
 
     def __repr__(self):
         s = []
-        s.append("nsteps = %d,"      % self.nsteps)
-        s.append("nfevals = %d,"     % self.nfevals)
-        s.append("njevals = %d.\n"     % self.njevals)
+        s.append("nsteps = %d," % self.nsteps)
+        s.append("nfevals = %d," % self.nfevals)
+        s.append("njevals = %d.\n" % self.njevals)
 
         return "(%s)" % ("\n".join(s))
 
@@ -238,4 +212,3 @@ cdef class CvodeSolver(object):
 
         N_VDestroy_Serial(self.u_y)
         CVodeFree(&self.cvode_mem)
-
