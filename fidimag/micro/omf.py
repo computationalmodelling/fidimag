5caaa133e12229c37d383a052a3a5f92c4a802b5
Thomas Kluyver <thomas@kluyver.me.uk>
2016-03-21 12:19:52 +0000

    Reading OVF files as binary

--- fidimag/micro/omf.py
+++ fidimag/micro/omf.py
@@ -20,21 +20,21 @@ class OMF2:
         f = open(self.file_name, 'rb')
         line = f.readline()
         # print line
-        if line != "# OOMMF OVF 2.0\n":
+        if line != b"# OOMMF OVF 2.0\n":
             print(self.file_name + ": NOT OOMMF OVF 2.0!")
             sys.exit(0)
 
-        while not line.startswith("# Begin: Data Binary"):
+        while not line.startswith(b"# Begin: Data Binary"):
             line = f.readline()
 
-            if line.startswith("# xnodes:"):
-                self.xnodes = int(line.split(":")[1])
-            elif line.startswith("# ynodes:"):
-                self.ynodes = int(line.split(":")[1])
-            elif line.startswith("# znodes:"):
-                self.znodes = int(line.split(":")[1])
-            elif line.startswith("# ystepsize:"):
-                self.ystepsize = float(line.split(":")[1])
+            if line.startswith(b"# xnodes:"):
+                self.xnodes = int(line.split(b":")[1])
+            elif line.startswith(b"# ynodes:"):
+                self.ynodes = int(line.split(b":")[1])
+            elif line.startswith(b"# znodes:"):
+                self.znodes = int(line.split(b":")[1])
+            elif line.startswith(b"# ystepsize:"):
+                self.ystepsize = float(line.split(b":")[1])
 
         msb = f.read(8)
 
