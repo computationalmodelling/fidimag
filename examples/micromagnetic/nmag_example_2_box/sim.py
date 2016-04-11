990b38a1048f1e3663ddf1be3b3751e9e41990e5
logicabrity <mail@marc-antonio.de>
2016-04-11 09:38:48 +0200

    add example 2 from the nmag manual

---
+++ examples/micromagnetic/nmag_example_2_box/sim.py
@@ -0,0 +1,21 @@
+"""
+example 2 in the nmag manual
+from http://nmag.soton.ac.uk/nmag/0.2/manual/html/example2/doc.html
+
+"""
+import numpy as np
+from fidimag.common import CuboidMesh
+from fidimag.micro import Sim, UniformExchange, Demag
+
+
+mesh = CuboidMesh(3, 3, 10/3.0, 10, 10, 30, unit_length=1e-9)
+sim = Sim(mesh)
+sim.Ms = 0.86e6
+sim.alpha = 0.5
+sim.set_m((1, 0, 1))
+sim.add(UniformExchange(A=13e-12))
+sim.add(Demag())
+
+ts = np.linspace(0, 3e-10, 61)
+for t in ts:
+    sim.run_until(t)
