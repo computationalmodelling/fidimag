ef2c751479aa57cabf3f3b77056f53bdb1aa8fb2
Hans Fangohr iota <fangohr@soton.ac.uk>
2016-04-14 17:05:18 +0100

    Add test to help with tracking down missing packages.

---
+++ tests/test_imports.py
@@ -0,0 +1,11 @@
+def test_has_pyvtk_installed():
+    # use 'pip install pyvtk' if this fails
+    import pyvtk
+
+def test_has_fidimag_installed():
+    # fix PYTHONPATH if this fails
+    import fidimag
+    
+def test_has_pytest_installed():
+    # install pytest if this fails (via conda, apt-get, ...)
+    import pytest
