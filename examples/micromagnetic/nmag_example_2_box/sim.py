52e7e71da524a26ff7135793029cb1810f9cb4e4
logicabrity <mail@marc-antonio.de>
2016-04-18 19:10:06 +0100

    run nmag example 2 with and without Jacobian activated

--- examples/micromagnetic/nmag_example_2_box/sim.py
+++ examples/micromagnetic/nmag_example_2_box/sim.py
@@ -7,15 +7,22 @@ import numpy as np
 from fidimag.common import CuboidMesh
 from fidimag.micro import Sim, UniformExchange, Demag
 
-
 mesh = CuboidMesh(3, 3, 10/3.0, 10, 10, 30, unit_length=1e-9)
-sim = Sim(mesh, name="nmag2")
-sim.Ms = 0.86e6
-sim.alpha = 0.5
-sim.set_m((1, 0, 1))
-sim.add(UniformExchange(A=13e-12))
-sim.add(Demag())
 
-ts = np.linspace(0, 3e-10, 61)
-for t in ts:
-    sim.run_until(t)
+
+def run(jacobian):
+    name = "sim_J" + ("1" if jacobian else "0")
+    sim = Sim(mesh, name, use_jac=jacobian)
+    sim.Ms = 0.86e6
+    sim.alpha = 0.5
+    sim.set_m((1, 0, 1))
+    sim.add(UniformExchange(A=13e-12))
+    sim.add(Demag())
+
+    ts = np.linspace(0, 3e-10, 61)
+    for t in ts:
+        sim.run_until(t)
+
+if __name__ == "__main__":
+    run(False)
+    run(True)
