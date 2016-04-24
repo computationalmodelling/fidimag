ca150b71d886d7cd05618e3d8aae5c30c4469292
Hans Fangohr <fangohr@soton.ac.uk>
2016-04-24 22:20:07 +0100

    better solution: mark full test as slow.

--- tests/test_domain_wall_cobalt.py
+++ tests/test_domain_wall_cobalt.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import pytest
 from fidimag.common import CuboidMesh
 from fidimag.micro import Sim, UniformExchange, UniaxialAnisotropy
 
@@ -65,10 +66,34 @@ def compute_domain_wall_cobalt(end_time):
     return sim.mesh.coordinates[:, 0], sim.spin
 
 
+@pytest.mark.slow
 def test_domain_wall_cobalt():
     
     # end_time = 1e-11
-    end_time = 1e-10
+    # end_time = 1e-10
+    end_time = 1e-9
+    xs, m = compute_domain_wall_cobalt(end_time=end_time)
+    m.shape = (-1, 3)
+    mz = m[:, 2]
+    # compare with analytical solution
+    ref = [reference_mz(x) for x in xs]
+    diff = np.max(np.abs(mz - ref))
+    print("max difference between simuelation and reference: ", diff)
+    if end_time == 1e-9:
+        accepted_deviation = 0.009996
+    elif end_time == 1e-10:
+        accepted_deviation = 0.096
+    elif end_time == 1e-11:
+        accepted_deviation = 0.63
+    else:
+        raise NotImplementedError(
+            "Don't know accepted deviation for end_time={}".format(end_time))
+
+
+def test_domain_wall_cobalt_fast():
+    
+    end_time = 1e-11
+    # end_time = 1e-10
     # end_time = 1e-9
     xs, m = compute_domain_wall_cobalt(end_time=end_time)
     m.shape = (-1, 3)
@@ -88,6 +113,8 @@ def test_domain_wall_cobalt():
             "Don't know accepted deviation for end_time={}".format(end_time))
 
 
+
+    
 if __name__ == '__main__':
     xs, m = compute_domain_wall_cobalt()
     m.shape = (-1, 3)
