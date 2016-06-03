dc21f5aebf5d989c51be7a3f590dcd326eea4ac3
Marc-Antonio Bisotti <mail@marc-antonio.de>
2016-06-03 15:48:19 +0200

    update ubuntu install script

--- bin/ubuntu_install_script.sh
+++ bin/ubuntu_install_script.sh
@@ -4,3 +4,4 @@ bash install-sundials.sh
 bash install-scikit-odes.sh
 make -C ..
 echo "export PYTHONPATH=$(dirname $PWD):\$PYTHONPATH" >> /home/$USER/.bashrc
+echo "export LD_LIBRARY_PATH=$(dirname $PWD)/local/lib:\$LD_LIBRARY_PATH" >> /home/$USER/.bashrc
