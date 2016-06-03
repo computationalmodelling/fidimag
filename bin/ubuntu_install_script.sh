193cd9e1919d8c7a1f850582287ffdaf5429a763
Marc-Antonio Bisotti <mail@marc-antonio.de>
2016-06-03 15:42:02 +0200

    add new install scripts to ubuntu_install_script.sh

--- bin/ubuntu_install_script.sh
+++ bin/ubuntu_install_script.sh
@@ -1,4 +1,6 @@
-bash install.sh
 sudo bash install-ubuntu-packages.sh
+bash install-fftw.sh
+bash install-sundials.sh
+bash install-scikit-odes.sh
 make -C ..
 echo "export PYTHONPATH=$(dirname $PWD):\$PYTHONPATH" >> /home/$USER/.bashrc
