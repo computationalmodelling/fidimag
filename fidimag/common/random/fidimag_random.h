d9728d0aca8a1f8098e766ff0f45e60534a70ec3
Weiwei Wang <wangww2011@gmail.com>
2016-06-21 12:23:15 +0800

    start to use new random number in sllg

--- fidimag/common/random/fidimag_random.h
+++ fidimag/common/random/fidimag_random.h
@@ -18,7 +18,11 @@ typedef struct {
 } mt19937_state;
 
 #define	MT19973_RAND_MAX 4294967295u
-void initial_rng_mt19973(mt19937_state *state);
+
+mt19937_state *create_mt19937_state(void);
+void finalize_mt19937_state(mt19937_state *state);
+
+void initial_rng_mt19973(mt19937_state *state, int seed);
 //inline unsigned int rand_int(mt19937_state *state);
 double random_double(mt19937_state *state);
 void gauss_random_vector(mt19937_state *state, double *x, int n);
