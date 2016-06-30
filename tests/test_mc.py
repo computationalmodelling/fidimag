2380d089e0de8669b1bd2582f076731ee99bcf53
Weiwei Wang <wangww2011@gmail.com>
2016-06-30 22:39:20 +0800

    add comments for the tests related to monte carlo

--- tests/test_mc.py
+++ tests/test_mc.py
@@ -7,22 +7,19 @@ import numpy as np
 from fidimag.atomistic import MonteCarlo
 
 
-def my_average(spin):
-    spin.shape = (-1,3)
-    n = spin.shape[0]
-    x1 = np.average([spin[i,0] for i in range(n) if spin[i,0]>0])
-    y1 = np.average([spin[i,1] for i in range(n) if spin[i,1]>0])
-    z1 = np.average([spin[i,2] for i in range(n) if spin[i,2]>0])
-    #print(x1, y1, z1)
-    return x1, y1, z1
-
 def test_random_sphere(do_plot=False):
+    """
+    test whether spins are uniformly distributed on the surface of a sphere.
+    """
     mt19937 = clib.rng_mt19937()
     mt19937.set_seed(123)
     spin = np.zeros(3 * 10000, dtype=np.float)
     mt19937.fill_vector_uniform_sphere(spin, 10000)
     spin.shape = (-1,3)
-    x,y,z = my_average(spin)
+    n = spin.shape[0]
+    x = np.average([spin[i,0] for i in range(n) if spin[i,0]>0])
+    y = np.average([spin[i,1] for i in range(n) if spin[i,1]>0])
+    z = np.average([spin[i,2] for i in range(n) if spin[i,2]>0])
     print(x,y,z)
     print(abs(x-y), abs(x-z), abs(y-z))
     assert(abs(x-y)<4e-3)
