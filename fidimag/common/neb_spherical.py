53d0deda3405980fa09c8827870874cf5fd66a35
davidcorteso <dic1e13@soton.ac.uk>
2016-07-12 23:32:33 +0100

    Fixed VTK saving. The save_vtk option needs to be simplified further

--- fidimag/common/neb_spherical.py
+++ fidimag/common/neb_spherical.py
@@ -548,8 +548,22 @@ class NEB_Sundials(object):
 
         self.coords.shape = (self.total_image_num, -1)
 
+        # We use Ms from the simulation assuming that all the
+        # images are the same
         for i in range(self.total_image_num):
-            self.vtk.save_vtk(spherical2cartesian(self.coords[i]), step=i, vtkname='m')
+            # We will try to save for the micromagnetic simulation (Ms)
+            # or an atomistic simulation (mu_s)
+            # TODO: maybe this can be done with an: isinstance
+            try:
+                self.vtk.save_vtk(spherical2cartesian(self.coords[i]).reshape(-1, 3),
+                                  self.sim.Ms,
+                                  step=i,
+                                  vtkname='m')
+            except:
+                self.vtk.save_vtk(spherical2cartesian(self.coords[i]).reshape(-1, 3),
+                                  self.sim._mu_s,
+                                  step=i,
+                                  vtkname='m')
 
         self.coords.shape = (-1, )
 
