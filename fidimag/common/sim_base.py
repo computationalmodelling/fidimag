d6aa72db93d880e6014b009bb56021006d4987fd
davidcortesortuno <david.cortes.o@gmail.com>
2016-08-05 15:29:54 +0100

    Created a common Simulation class with shared methods and definitions (only works with micro simulations for now). Massively documented Simulation class methods

---
+++ fidimag/common/sim_base.py
@@ -0,0 +1,332 @@
+import fidimag.common.helper as helper
+
+import numpy as np
+import os
+
+
+class SimBase(object):
+    """
+
+    A class with common methods and definitions for both micromagnetic and
+    atomistic simulations
+
+    """
+    def __init__(self, mesh, name):
+
+        self.name = name
+        self.mesh = mesh
+        self.n = mesh.n
+        self.n_nonzero = mesh.n
+        self.unit_length = mesh.unit_length
+
+        # self._Ms = np.zeros(self.n, dtype=np.float)
+        self.spin = np.ones(3 * self.n, dtype=np.float)
+        self._pins = np.zeros(self.n, dtype=np.int32)
+        self.field = np.zeros(3 * self.n, dtype=np.float)
+        self._skx_number = np.zeros(self.n, dtype=np.float)
+
+        # This is for old C files codes using the xperiodic variables
+        self.xperiodic, self.yperiodic, self.zperiodic = mesh.periodicity
+
+    def set_m(self, m0=(1, 0, 0),
+              normalise=True):
+        """
+
+        Set the magnetisation/spin three dimensional vector field profile.
+
+        ARGUMENTS:
+
+        m0      :: * To set every spin with the same direction,
+                   set this value as a 3 elements tuple or list.
+
+                   * For a spatially dependent vector field, you can specify a
+                   function that returns a 3 element list depending on the
+                   spatial coordinates. For example, a magnetisation field that
+                   depends on the x position:
+
+                        def m_profile(r):
+                            for r[0] > 2:
+                                return (0, 0, 1)
+                            else:
+                                return (0, 0, -1)
+
+                   * You can also manually specify an array with (3 * n)
+                   elements with the spins directions in the following order:
+
+                        [mx_0 my_0 mz_0 mx_1 my_1 ... mx_n, my_n, mz_n]
+
+                   where n is the number of mesh nodes and the order of the
+                   magnetisation vectors follow the same order than the mesh
+                   coordinates array.
+
+                   * Alternatively, if you previously saved the magnetisation
+                   field array to a numpy file, you can load it using
+                   numpy.load(my_array)
+
+        """
+
+        self.spin[:] = helper.init_vector(m0, self.mesh, normalise)
+
+        # TODO: carefully checking and requires to call set_mu first
+
+        # Set the magnetisation/spin direction to (0, 0, 0) for sites
+        # with no material, i.e. M_s = 0 or mu_s = 0
+        # TODO: Check for atomistic and micromagnetic cases
+        self.spin.shape = (-1, 3)
+        for i in range(self.spin.shape[0]):
+            if self._Ms[i] == 0:
+                self.spin[i, :] = 0
+        self.spin.shape = (-1,)
+
+        # Set the initial state for the Sundials integrator using the
+        # spins array
+        self.integrator.set_initial_value(self.spin, self.t)
+
+    def get_pins(self):
+        """
+        Returns the array with pinned spins in the sample:
+        sites with 0 are unpinned and sites with 1 are pinned. The order
+        of the array follows the same order than the mesh coordinates
+        """
+        return self._pins
+
+    def set_pins(self, pin):
+        """
+
+        An scalar field with values 1 or 0 to specify mesh/lattice sites
+        with pinned or unpinned magnetic moments, respectively
+
+        ARGUMENTS:
+
+        pin     :: * You can specify a function that returns 1 or 0 depending
+                   on the spatial coordinates. For example, to pin the spins
+                   in a range in the x direction:
+
+                        def pin_profile(r):
+                            for r[0] > 2 and r[0] < 4:
+                                return 1
+                            else:
+                                return 0
+
+                   * You can also manually specify an array with n elements (1
+                   or 0) with the pinned/unpinned values in the same order than
+                   the mesh coordinates array.
+
+                   * Alternatively, if you previously saved the pin
+                   field array to a numpy file, you can load it using
+                   numpy.load(my_array)
+        """
+        self._pins[:] = helper.init_scalar(pin, self.mesh)
+
+        # Sites with no material, i.e. Mu_s or mu_s equal to zero,
+        # will be pinned
+        for i in range(len(self._Ms)):
+            if self._Ms[i] == 0.0:
+                self._pins[i] = 1
+
+    pins = property(get_pins, set_pins)
+
+    def get_alpha(self):
+        """
+        Returns the array with the spatially dependent Gilbert damping
+        per mesh/lattice site
+        """
+        return self._alpha
+
+    def set_alpha(self, value):
+        """
+
+        Set the Gilbert damping of the system as a uniform or spatially
+        dependent scalar field
+
+        ARGUMENTS:
+
+        value     :: * For a uniform damping across the whole sample, just
+                       specify a float ranging from 0 to 1
+
+                     * In addition, you can specify a function that returns
+                       values ranging from 0 to 1, which depends on the spatial
+                       coordinates. For example, a damping that increases
+                       linearly in the x direction:
+
+                        def alpha_profile(r):
+                            for r[0] <= 10:
+                                return r[0] / 10.
+                            else:
+                                return 0
+
+                     * You can also manually specify an array with n values
+                     ranging from 0 to 1 with the damping values, in the same
+                     order than the mesh coordinates array.
+
+                     * Alternatively, if you previously saved the damping
+                     field array to a numpy file, you can load it using
+                     numpy.load(my_array)
+        """
+        self._alpha[:] = helper.init_scalar(value, self.mesh)
+
+    alpha = property(get_alpha, set_alpha)
+
+    def add(self, interaction, save_field=False):
+        """
+
+        Add an interaction from one of the Energy subclasses. By default,
+        the average energy of the added interaction is saved to the
+        data file when relaxing the system
+
+        OPTIONAL ARGUMENTS:
+
+        save_field      :: Set True to save the average values of this
+                           interaction field when relaxing the system
+
+        """
+
+        interaction.setup(self.mesh, self.spin, Ms=self._Ms)
+
+        # TODO: FIX  --> ??
+        # When adding an interaction that was previously added, using
+        # the same name, append a '_2' to the new interaction name (?)
+        for i in self.interactions:
+            if i.name == interaction.name:
+                interaction.name = i.name + '_2'
+
+        self.interactions.append(interaction)
+
+        # Specify a name for the energy of the interaction, which will
+        # appear in a file with saved values
+        # When saving the energy values, we call the compute_energy() method
+        # from the (micromagnetic/atomistic) Energy class (overhead?)
+        energy_name = 'E_{0}'.format(interaction.name)
+        self.saver.entities[energy_name] = {
+            'unit': '<J>',
+            'get': lambda sim: sim.get_interaction(interaction.name).compute_energy(),
+            'header': energy_name}
+
+        # Save the average values of the interaction vector field components
+        if save_field:
+            fn = '{0}'.format(interaction.name)
+            self.saver.entities[fn] = {
+                'unit': '<>',
+                'get': lambda sim: sim.get_interaction(interaction.name).average_field(),
+                'header': ('%s_x' % fn, '%s_y' % fn, '%s_z' % fn)}
+
+        self.saver.update_entity_order()
+
+    def get_interaction(self, name):
+        """
+        Returns an instance of a magnetic interaction previously added
+        to the simulation, using the corresponding interaction name as
+        a string
+        """
+        for interaction in self.interactions:
+            if interaction.name == name:
+                return interaction
+        else:
+            raise ValueError("Failed to find the interaction with name '{0}', "
+                             "available interactions: {1}.".format(
+                                 name, [x.name for x in self.interactions]))
+
+    def compute_average(self):
+        """
+        Compute the average values of the 3 components of the magnetisation
+        vector field
+        """
+        self.spin.shape = (-1, 3)
+        average = np.sum(self.spin, axis=0) / self.n_nonzero
+        self.spin.shape = (3 * self.n)
+        return average
+
+    def compute_energy(self):
+        """
+        Compute the total energy of the magnetic system
+        """
+        energy = 0
+
+        for obj in self.interactions:
+            energy += obj.compute_energy()
+
+        return energy
+
+    def skyrmion_number(self):
+        pass
+
+    def spin_at(self, i, j, k):
+        """
+        Returns the x,y,z components of a spin in the [i, j, k]
+        position of the mesh, where i,j,k are integer indexes. The index
+        ordering is specified in the mesh class.
+        """
+
+        i1 = 3 * self.mesh.index(i, j, k)
+
+        # print self.spin.shape,nxy,nx,i1,i2,i3
+        return np.array([self.spin[i1],
+                         self.spin[i1 + 1],
+                         self.spin[i1 + 2]])
+
+    def add_monitor_at(self, i, j, k, name='p'):
+        """
+        Save site spin with index (i,j,k) to txt file.
+        """
+
+        self.saver.entities[name] = {
+            'unit': '<>',
+            'get': lambda sim: sim.spin_at(i, j, k),
+            'header': (name + '_x', name + '_y', name + '_z')}
+
+        self.saver.update_entity_order()
+
+    def save_vtk(self):
+        """
+        Save a VTK file with the magnetisation vector field (vector data)
+        and the saturation magnetisation values (scalar data) as
+        cell data
+        """
+        self.vtk.save_vtk(self.spin.reshape(-1, 3), self.Ms, step=self.step)
+
+    def save_m(self):
+        """
+        Save the magnetisation/spin vector field as a numpy array in
+        a NPY file. The files are saved in the `{name}_npys` folder, where
+        `{name}` is the simulation name, with the file name `m_{step}.npy`
+        where `{step}` is the simulation step (from the integrator)
+        """
+        if not os.path.exists('%s_npys' % self.name):
+            os.makedirs('%s_npys' % self.name)
+        name = '%s_npys/m_%g.npy' % (self.name, self.step)
+        np.save(name, self.spin)
+
+    def save_skx(self):
+        """
+        Save the skyrmion number density (sk number per mesh site)
+        as a numpy array in a NPY file.
+        The files are saved in the `{name}_skx_npys` folder, where
+        `{name}` is the simulation name, with the file name `skx_{step}.npy`
+        where `{step}` is the simulation step (from the integrator)
+        """
+        if not os.path.exists('%s_skx_npys' % self.name):
+            os.makedirs('%s_skx_npys' % self.name)
+        name = '%s_skx_npys/m_%g.npy' % (self.name, self.step)
+        np.save(name, self._skx_number)
+
+    def stat(self):
+        return self.integrator.stat()
+
+    def spin_length(self):
+        self.spin.shape = (-1, 3)
+        length = np.sqrt(np.sum(self.spin**2, axis=1))
+        self.spin.shape = (-1,)
+        return length
+
+    def compute_spin_error(self):
+        length = self.spin_length() - 1.0
+        length[self._pins > 0] = 0
+        return np.max(abs(length))
+
+    def compute_dmdt(self, dt):
+        m0 = self.spin_last
+        m1 = self.spin
+        dm = (m1 - m0).reshape((3, -1))
+        max_dm = np.max(np.sqrt(np.sum(dm**2, axis=0)))
+        max_dmdt = max_dm / dt
+        return max_dmdt
--- fidimag/micro/sim.py
+++ fidimag/micro/sim.py
@@ -1,9 +1,15 @@
+import fidimag
+
 from . import llg
 from . import llg_stt
 from . import llg_stt_cpp
 from . import baryakhtar
 
 from . import micro_driver
+import fidimag.extensions.micro_clib as micro_clib
+import fidimag.common.helper as helper
+
+from fidimag.common import SimBase
 
 import numpy as np
 
@@ -14,86 +20,97 @@ KNOWN_DRIVERS = {'llg': llg.LLG,
                  'llbar_full': baryakhtar.LLBarFull}
 
 
-class Sim(object):
+class Sim(SimBase):
     """
-    def Sim(*args, **kwargs)
-    
-    This function returns a simulation object.
 
-    By default, it will return a simulation object for the LLG driver;
-    other available drivers are:
-        llg_stt - LLG w. spin transfer torque
-        llg_stt_cpp - LLG w. spin transfer torque 
-        llbar - Landau-Lifshitz-Baryakhtar equation
-        llbar_full
-    
-    To use the STT driver, for example, pass as an argument driver="llg_stt".
+    The Simulation class for a micromagnetic simulation.
+
+    ARGUMENTS:
+
+    mesh    :: A CuboidMesh instance, from fidimag.common
+
+    OPTIONAL ARGUMENTS:
+
+    name    :: A string with the simulation name
+    driver  :: A string with one of the following drivers to evolve
+               the micromagnetic system:
+
+                    llg         - (default) the Landau-Lifshitz-Gilbert
+                                  equation
+                    llg_stt     - LLG w. spin transfer torque
+                    llg_stt_cpp - LLG w. spin transfer torque perpendicular
+                                  to the plane (?)
+                    llbar       - Landau-Lifshitz-Baryakhtar equation
+                    llbar_full
+
+    ** Most of the methods inherit from the Base Simulation class that
+    can be found in the common folder
 
     """
-    def __init__(self, mesh,
-                 driver='llg', 
-                 *args, **kwargs
-                 ):
+
+    def __init__(self, mesh, name='unnamed', driver='llg'):
+
+        super(Sim, self).__init__(mesh, name)
+
         self._micromagnetic = True
 
-        self.name = name
-        self.mesh = mesh
-        self.n = mesh.n
-        self.n_nonzero = mesh.n
-        self.unit_length = mesh.unit_length
-
-
         self._Ms = np.zeros(self.n, dtype=np.float)
-        self.spin = np.ones(3 * self.n, dtype=np.float)
-        self._pins = np.zeros(self.n, dtype=np.int32)
-        self.field = np.zeros(3 * self.n, dtype=np.float)
 
-        self._skx_number = np.zeros(self.n, dtype=np.float)
+        if driver not in KNOWN_DRIVERS:
+            raise NotImplementedError("""Driver '{}' is not implemented.
+                                      Valid choices: one of '{}'.""".format(driver, KNOWN_DRIVERS.keys()))
 
-        # This is for old C files codes using the xperiodic variables
-        self.xperiodic, self.yperiodic, self.zperiodic = mesh.periodicity
-
-        # 
-        self.driver = KNOWN_DRIVERS[driver](*args, **kwargs)
-
-
-    def set_m(self, m0=(1, 0, 0), normalise=True):
-
-        self.spin[:] = helper.init_vector(m0, self.mesh, normalise)
-
-        # TODO: carefully checking and requires to call set_mu first
-        self.spin.shape = (-1, 3)
-        for i in range(self.spin.shape[0]):
-            if self._Ms[i] == 0:
-                self.spin[i, :] = 0
-        self.spin.shape = (-1,)
-
-        self.integrator.set_initial_value(self.spin, self.t)
-
-    def get_pins(self):
-        return self._pins
-
-    def set_pins(self, pin):
-        self._pins[:] = helper.init_scalar(pin, self.mesh)
-
-        for i in range(len(self._Ms)):
-            if self._Ms[i] == 0.0:
-                self._pins[i] = 1
-
-    pins = property(get_pins, set_pins)
-
-    def get_alpha(self):
-        return self._alpha
-
-    def set_alpha(self, value):
-        self._alpha[:] = helper.init_scalar(value, self.mesh)
-
-    alpha = property(get_alpha, set_alpha)
+        self.driver = KNOWN_DRIVERS[driver](self.mesh,
+                                            self.spin,
+                                            self.field,
+                                            self._pins,
+                                            )
 
     def get_Ms(self):
+        """
+        Returns the array with the saturation magnetisation values per
+        mesh site
+        """
         return self._Ms
 
     def set_Ms(self, value):
+        """
+
+        Set the saturation magnetisation of the system as a uniform or
+        spatially dependent scalar field, where values are in A /m.  Mesh sites
+        with no material can be specified with a magnetisation of zero
+        magnitude. Samples with different materials can be specified with
+        different magnetisation values in specific regions of the system.
+
+        ARGUMENTS:
+
+        value     :: * For a homogeneous single material sample, specify a
+                       float with a magnitude in A /m
+
+                     * In addition, you can specify a function that returns
+                       values in A /m, which depends on the spatial
+                       coordinates. For example, a 2 nm wide cylinder centered
+                       at (x, y) = (1, 1) can be specified with:
+
+                            Ms = 1e6  # A / m
+
+                            def Ms_profile(r):
+                                for (r[0] - 1) ** 2 + (r[1] - 1) ** 2 <= 1 ** 2:
+                                    return Ms
+                                else:
+                                    return 0
+
+                            Sim.set_Ms(Ms_profile)
+
+                     * You can also manually specify an array with n values
+                     ranging from 0 to 1 with the magnetisation values, in the
+                     same order than the mesh coordinates array.
+
+                     * Alternatively, if you previously saved the damping
+                     field array to a numpy file, you can load it using
+                     numpy.load(my_array).
+        """
+
         self._Ms[:] = helper.init_scalar(value, self.mesh)
         nonzero = 0
         for i in range(self.n):
@@ -114,56 +131,11 @@ class Sim(object):
 
     Ms = property(get_Ms, set_Ms)
 
-    def add(self, interaction, save_field=False):
-        interaction.setup(self.mesh, self.spin, Ms=self._Ms)
-
-        # TODO: FIX
-        for i in self.interactions:
-            if i.name == interaction.name:
-                interaction.name = i.name + '_2'
-
-        self.interactions.append(interaction)
-
-        energy_name = 'E_{0}'.format(interaction.name)
-        self.saver.entities[energy_name] = {
-            'unit': '<J>',
-            'get': lambda sim: sim.get_interaction(interaction.name).compute_energy(),
-            'header': energy_name}
-
-        if save_field:
-            fn = '{0}'.format(interaction.name)
-            self.saver.entities[fn] = {
-                'unit': '<>',
-                'get': lambda sim: sim.get_interaction(interaction.name).average_field(),
-                'header': ('%s_x' % fn, '%s_y' % fn, '%s_z' % fn)}
-
-        self.saver.update_entity_order()
-
-    def get_interaction(self, name):
-        for interaction in self.interactions:
-            if interaction.name == name:
-                return interaction
-        else:
-            raise ValueError("Failed to find the interaction with name '{0}', "
-                             "available interactions: {1}.".format(
-                                 name, [x.name for x in self.interactions]))
-
-    def compute_average(self):
-        self.spin.shape = (-1, 3)
-        average = np.sum(self.spin, axis=0) / self.n_nonzero
-        self.spin.shape = (3 * self.n)
-        return average
-
-    def compute_energy(self):
-
-        energy = 0
-
-        for obj in self.interactions:
-            energy += obj.compute_energy()
-
-        return energy
-
     def skyrmion_number(self):
+        """
+        Returns the skyrmion number from the first layer in the
+        XY plane of the mesh
+        """
         nx = self.mesh.nx
         ny = self.mesh.ny
         nz = self.mesh.nz
@@ -173,74 +145,3 @@ class Sim(object):
 
     skyrmion_number_slice = fidimag.common.skyrmion_number.skyrmion_number_slice
     skyrmion_number_lee = fidimag.common.skyrmion_number.skyrmion_number_lee
-
-    def spin_at(self, i, j, k):
-
-        i1 = 3 * self.mesh.index(i, j, k)
-
-        # print self.spin.shape,nxy,nx,i1,i2,i3
-        return np.array([self.spin[i1],
-                         self.spin[i1 + 1],
-                         self.spin[i1 + 2]])
-
-    def add_monitor_at(self, i, j, k, name='p'):
-        """
-        Save site spin with index (i,j,k) to txt file.
-        """
-
-        self.saver.entities[name] = {
-            'unit': '<>',
-            'get': lambda sim: sim.spin_at(i, j, k),
-            'header': (name + '_x', name + '_y', name + '_z')}
-
-        self.saver.update_entity_order()
-
-    def save_vtk(self):
-        self.vtk.save_vtk(self.spin.reshape(-1, 3), self.Ms, step=self.step)
-
-    def save_m(self):
-        if not os.path.exists('%s_npys' % self.name):
-            os.makedirs('%s_npys' % self.name)
-        name = '%s_npys/m_%g.npy' % (self.name, self.step)
-        np.save(name, self.spin)
-
-    def save_skx(self):
-        if not os.path.exists('%s_skx_npys' % self.name):
-            os.makedirs('%s_skx_npys' % self.name)
-        name = '%s_skx_npys/m_%g.npy' % (self.name, self.step)
-        np.save(name, self._skx_number)
-
-    def stat(self):
-        return self.integrator.stat()
-
-    def spin_length(self):
-        self.spin.shape = (-1, 3)
-        length = np.sqrt(np.sum(self.spin**2, axis=1))
-        self.spin.shape = (-1,)
-        return length
-
-    def compute_spin_error(self):
-        length = self.spin_length() - 1.0
-        length[self._pins > 0] = 0
-        return np.max(abs(length))
-
-    def compute_dmdt(self, dt):
-        m0 = self.spin_last
-        m1 = self.spin
-        dm = (m1 - m0).reshape((3, -1))
-        max_dm = np.max(np.sqrt(np.sum(dm**2, axis=0)))
-        max_dmdt = max_dm / dt
-        return max_dmdt
-
-def Sim(*args, **kwargs):
-
-    driver = 'llg'
-
-    if 'driver' in kwargs:
-        driver = kwargs.pop('driver')
-
-    if driver not in KNOWN_DRIVERS:
-        raise NotImplementedError("""Driver '{}' is not implemented.
-                                  Valid choices: one of '{}'.""".format(driver, KNOWN_DRIVERS.keys()))
-
-    return KNOWN_DRIVERS[driver](*args, **kwargs)
