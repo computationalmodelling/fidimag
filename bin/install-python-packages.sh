5ef6d96cacfb30c82993bd4eacc20354f6d39b32
logicabrity <mail@marc-antonio.de>
2016-08-09 13:33:43 +0100

    outsource installation with pip to own script

---
+++ bin/install-python-packages.sh
@@ -0,0 +1,1 @@
+pip install --upgrade cython psutil ipywidgets
--- bin/ubuntu_install_script.sh
+++ bin/ubuntu_install_script.sh
@@ -1,15 +1,13 @@
 echo "Super user authentication required to add packages (you will be \
 prompted for confirmation)."
 sudo bash install-ubuntu-packages.sh
+sudo bash install-python-packages.sh
 bash install-fftw.sh
 bash install-sundials.sh
-sudo pip install cython --upgrade
-sudo pip install psutil ipywidgets
 pushd .. > /dev/null
 make
 
-# Adds Fidimag environment variables to profile.d, if they're not already
-# there.
+# Adds fidimag environment variables to profile.d, if they're not already there.
 FIDIMAG_PROFILE_PATH=/etc/profile.d/fidimag.sh
 if [ ! -e "$FIDIMAG_PROFILE_PATH" ]; then
     echo "Warning: Adding Fidimag to path at $FIDIMAG_PROFILE_PATH."
