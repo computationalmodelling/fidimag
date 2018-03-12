330da53f32f3fe0b41707a6735e60f10d455df36
davidcorteso <d.i.cortes@soton.ac.uk>
2018-03-12 15:25:13 +0000

    Started implementation of steepest descent in atomistic code

---
+++ fidimag/atomistic/steepest_descent.py
@@ -0,0 +1,123 @@
+from __future__ import division
+import numpy as np
+import fidimag.extensions.clib as clib
+import fidimag.common.helper as helper
+import fidimag.common.constant as const
+
+from .atomistic_driver import AtomisticDriver
+
+
+class SteepestDescent(AtomisticDriver):
+    """
+
+    This class is the driver to minimise a system using a Steepest Descent
+    algorithm
+
+
+
+    This class inherits common methods to evolve the system using CVODE, from
+    the micro_driver.AtomisticDriver class. Arrays with the system information
+    are taken as references from the main micromagnetic Simulation class
+
+    """
+
+    def __init__(self, mesh, spin, mu_s, mu_s_inv, field, pins,
+                 interactions,
+                 name,
+                 data_saver,
+                 ):
+
+        # self.mesh = mesh
+        # self.spin = spin
+        # self.mu_s mu_s
+        # self._mu_s_inv = mu_s_inv
+        # self.field = field
+        # self.pins = pins
+        # self.interactions = interactions
+        # self.name = name
+        # self.data_saver = data_saver
+
+        # Inherit from the driver class
+        super(SteepestDescent, self).__init__(mesh, spin, mu_s, mu_s_inv, field,
+                                              pins, interactions, name,
+                                              data_saver,
+                                              # use_jac=use_jac,
+                                              # integrator=integrator
+                                              )
+
+        self.mxH = np.zeros_like(self.field)
+        self.mxmxH = np.zeros_like(self.field)
+        self.mxmxH_last = np.zeros_like(self.field)
+        self.t = 1e-4
+
+        # self.set_options()
+
+    def field_cross_product(self, a, b):
+        aXb = np.cross(a(-1, 3), b(-1, 3))
+        return aXb.reshape(-1,)
+
+    def get_time_step(self):
+        ds = self.spin - self.spin_last
+        dy = self.mxmxH - self.mxmxH_last
+
+        if self.counter % 2 == 0:
+            num = np.dot(ds, ds)
+            den = np.dot(ds, dy)
+        else:
+            num = np.dot(ds, dy)
+            den = np.dot(dy, dy)
+
+        if den != 0:
+            return num / den
+        else:
+            return 1e-4
+
+    def compute_rhs(self, t):
+
+        den = 4 + 
+
+    def run_step(self):
+
+        self.update_effective_field(self.spin, self.t)
+        self.mxH = self.field_cross_product(self.spin, self.field)
+        self.mxmxH = self.field_cross_product(self.spin, self.mxH)
+        self.t = self.get_time_step()
+
+        
+
+        clib.normalise_spin(self.spin, self._pins, self.n)
+
+
+    def update_effective_field(self, y, t):
+
+        self.field[:] = 0
+
+        for obj in self.interactions:
+            self.field += obj.compute_field(t, spin=y)
+
+    def minimize(self, max_dmdt=1e-2):
+        self.counter = 0
+
+        while dmdt:
+            self.spin_last[:] = self.spin[:]
+            self.run_step()
+            self.dmdt = self.compute_dmdt(self.t)
+
+            if dmdt < stopping_dmdt * self._dmdt_factor:
+                break
+            
+            self.counter = 0
+
+    def run_until(self, t):
+
+        self.counter = 0
+
+        self.spin_last[:] = self.spin[:]
+
+        while (self.t < t):
+            self.run_step()
+        self.step += 1
+
+        # update field before saving data
+        self.compute_effective_field(t)
+        self.data_saver.save()
