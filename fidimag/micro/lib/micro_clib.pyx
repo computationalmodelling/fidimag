cba983d28b89a23a6bd7bf07f9b5e95b8ec3fb4f
Ryan Pepper <ryan.pepper@soton.ac.uk>
2018-05-02 15:52:20 +0100

    Change micro_clib to use Cython buffer interface

--- fidimag/micro/lib/micro_clib.pyx
+++ fidimag/micro/lib/micro_clib.pyx
@@ -25,34 +25,34 @@ cdef extern from "micro_clib.h":
                            int nx, int ny, int nz, int *ngbs)
 
 
-def compute_exchange_field_micro(double m[:],
-                                 double field[:],
-                                 double energy[:],
-                                 double Ms_inv[:],
+def compute_exchange_field_micro(double[:] m,
+                                 double[:] field,
+                                 double[:] energy[:],
+                                 double[:] Ms_inv[:],
                                  A, dx, dy, dz, n,
                                  int ngbs[:, :]):
     compute_exch_field_micro(&m[0], &field[0], &energy[0], &Ms_inv[0], A,
                              dx, dy, dz, n, &ngbs[0, 0])
 
 
-def compute_exchange_field_micro_rkky(double m[:],
-                                 double field[:],
-                                 double energy[:],
-                                 double Ms_inv[:],
+def compute_exchange_field_micro_rkky(double[:] m[:],
+                                 double[:] field[:],
+                                 double[:] energy[:],
+                                 double[:] Ms_inv[:],
                                  sigma, nx, ny, nz, z_bottom, z_top):
     compute_exch_field_rkky_micro(&m[0], &field[0], &energy[0], &Ms_inv[0], sigma,
                              nx, ny, nz, z_bottom, z_top)
     
 
-def compute_dmi_field(np.ndarray[double, ndim=1, mode="c"] m,
-                      np.ndarray[double, ndim=1, mode="c"] field,
-                      np.ndarray[double, ndim=1, mode="c"] energy,
-                      np.ndarray[double, ndim=1, mode="c"] Ms_inv,
-                      np.ndarray[double, ndim=1, mode="c"] D,
-                      np.ndarray[double, ndim=1, mode="c"] dmi_vector,
+def compute_dmi_field(double[:] m,
+                      double[:] field,
+                      double[:] energy,
+                      double[:] Ms_inv,
+                      double[:] D,
+                      double[:] dmi_vector,
                       n_dmi_ngbs,
                       dx, dy, dz,
-                      n, np.ndarray[int, ndim=2, mode="c"] ngbs
+                      n, int[:, :] ngbs
                       ):
 
     dmi_field(&m[0], &field[0], &energy[0], &Ms_inv[0], 
@@ -60,20 +60,20 @@ def compute_dmi_field(np.ndarray[double, ndim=1, mode="c"] m,
               dx, dy, dz, n, &ngbs[0, 0])
     
 
-def compute_anisotropy_micro(np.ndarray[double, ndim=1, mode="c"] m,
-                            np.ndarray[double, ndim=1, mode="c"] field,
-                            np.ndarray[double, ndim=1, mode="c"] energy,
-                            np.ndarray[double, ndim=1, mode="c"] Ms_inv,
-                            np.ndarray[double, ndim=1, mode="c"] Ku,
-                            np.ndarray[double, ndim=1, mode="c"] axis,
+def compute_anisotropy_micro(double[:] m,
+                            double[:] field,
+                            double[:] energy,
+                            double[:] Ms_inv,
+                            double[:] Ku,
+                            double[:] axis,
                             nx, ny, nz):
 
     compute_uniaxial_anis(&m[0], &field[0], &energy[0], &Ms_inv[0],
                           &Ku[0], &axis[0], nx, ny, nz)
 
-def compute_skyrmion_number(np.ndarray[double, ndim=1, mode="c"] m,
-                            np.ndarray[double, ndim=1, mode="c"] charge,
+def compute_skyrmion_number(double[:] m,
+                            double[:] charge,
                             nx, ny, nz,
-            		    np.ndarray[int, ndim=2, mode="c"] ngbs):
+            		            int[:, :] ngbs):
 
     return skyrmion_number(&m[0], &charge[0], nx, ny, nz, &ngbs[0, 0])
