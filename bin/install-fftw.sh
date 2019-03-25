fac181913ff4c320e6ddf2745a7c01df848bdb46
Ryan Pepper <ryan.pepper@soton.ac.uk>
2019-03-25 14:12:02 +0000

    Switch FFTW compilation to quiet

--- bin/install-fftw.sh
+++ bin/install-fftw.sh
@@ -33,7 +33,7 @@ download_and_install() {
         tar -xzf ${1}.tar.gz
         cd ${1}
         echo "Configuring "${1}"."
-        ./configure --enable-shared --enable-openmp --enable-sse2 --enable-avx --prefix=${LIBS_DIR}
+        ./configure --quiet --enable-shared --enable-openmp --enable-sse2 --enable-avx --prefix=${LIBS_DIR}
         echo "Compiling and installing "${1}"."
         make
         make install
