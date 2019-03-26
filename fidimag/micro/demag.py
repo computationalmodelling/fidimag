22cf337dd9e1095f2f124959f1925582dd8871ab
Ryan Pepper <ryan.pepper@soton.ac.uk>
2019-03-26 13:40:23 +0000

    Disable saving of demag tensor which led to bug

--- fidimag/micro/demag.py
+++ fidimag/micro/demag.py
@@ -50,26 +50,25 @@ class Demag(Energy):
                 asymptotic_radius = options['asymptotic_radius']
             if 'dipolar_radius' in options:
                 dipolar_radius = options['dipolar_radius']
-            if 'tensor_file_name' in options:
-                tensor_file_name = options['tensor_file_name']
+            #if 'tensor_file_name' in options:
+            #    tensor_file_name = options['tensor_file_name']
 
-            if tensor_file_name:
-                if not (os.path.exists(tensor_file_name+'.npz')):
-                    self.demag.compute_tensors_2dpbc(tensors, pbc_2d_error, sample_repeat_nx, sample_repeat_ny, dipolar_radius)
-                else:
-                    npzfile = np.load(tensor_file_name+'.npz')
-                    geo_info = npzfile['geo']
-                    geo_arr = np.array([self.nx,self.ny, self.nz, self.dx, self.dy, self.dz], dtype=np.float)
-                    #print 'info', geo_info
-                    if not np.allclose(geo_arr, geo_info):
-                        self.demag.compute_tensors_2dpbc(tensors, pbc_2d_error, sample_repeat_nx, sample_repeat_ny, dipolar_radius)
-                    else:
-                        tensors = npzfile['tensors']
-                geo_arr = np.array([self.nx, self.ny, self.nz, self.dx, self.dy, self.dz], dtype=np.float)
-                np.savez(tensor_file_name+'.npz', geo=geo_arr, tensors=tensors)
+            #if tensor_file_name:
+                #if not (os.path.exists(tensor_file_name+'.npz')):
+                #    self.demag.compute_tensors_2dpbc(tensors, pbc_2d_error, sample_repeat_nx, sample_repeat_ny, dipolar_radius)
+                #else:
+                #    npzfile = np.load(tensor_file_name+'.npz')
+                #    geo_info = npzfile['geo']
+                #    geo_arr = np.array([self.nx,self.ny, self.nz, self.dx, self.dy, self.dz], dtype=np.float)
+                #    #print 'info', geo_info
+                #    if not np.allclose(geo_arr, geo_info):
+                #        self.demag.compute_tensors_2dpbc(tensors, pbc_2d_error, sample_repeat_nx, sample_repeat_ny, dipolar_radius)
+                #    else:
+                #        tensors = npzfile['tensors']
+                #geo_arr = np.array([self.nx, self.ny, self.nz, self.dx, self.dy, self.dz], dtype=np.float)
+                #np.savez(tensor_file_name+'.npz', geo=geo_arr, tensors=tensors)
 
-            else:
-                self.demag.compute_tensors_2dpbc(tensors, pbc_2d_error, sample_repeat_nx, sample_repeat_ny, dipolar_radius)
+            self.demag.compute_tensors_2dpbc(tensors, pbc_2d_error, sample_repeat_nx, sample_repeat_ny, dipolar_radius)
 
             #print tensors
             self.demag.fill_demag_tensors(tensors)
