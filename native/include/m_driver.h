eaccbf5ddd68f1c965ce8e97897bfa93563fae15
davidcortesortuno <david.cortes.o@gmail.com>
2019-07-29 13:44:00 +0100

    Starting implementation of micro driver

--- native/include/m_driver.h
+++ native/include/m_driver.h
@@ -9,4 +9,19 @@ public:
     virtual ~Driver() {std::cout << "Killing base Driver\n";};
     // Will get the parameters from a simulation class
     void _setup(MicroSim * sim);
+
+    double * alpha;
+    double gamma;
+    double t;
+};
+
+
+class Integrator_RK4 {
+public:
+    Integrator_RK4() {};
+    virtual ~Integrator_RK4() {std::cout << "Killing RK4 integrator\n";};
+    // Will get the parameters from a simulation class
+    // void _setup(MicroSim * sim, Driver * driver);
+    std::vector<double> rk_steps;  // N * 4 array
+    void integration_step(double (*f)(double t, double y));
 };
