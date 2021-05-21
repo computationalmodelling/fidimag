1ff7c485932ea0f3e15f3dbdc40e3e296f0ee5c1
davidcortesortuno <david.cortes.o@gmail.com>
2021-05-21 19:29:36 +0200

    Driver problem: pass RHS fun to integrator

--- native/include/m_driver.h
+++ native/include/m_driver.h
@@ -2,22 +2,61 @@
 #include<iostream>
 #include "c_micro_sim.h"
 
+enum IntegratorID {
+    RK4
+};
+
+// In case we want to use conditional statements for LLG eq:
+// enum LLGTerms {
+//     PRECESSION,
+//     DAMPING,
+//     STT_ZHANGLI,
+//     STT_SLONCZEWSKI,
+//     STT_FIELDLIKE
+// };
+
+class RHS_function {
+public:
+    void compute_RHS(double * m, unsigned int n, double t);
+};
+
+
 class Integrator;  // forward declaration
 // Base class for the drivers -> just declare LLG driver for now
 class MicroLLGDriver {
 public:
     MicroLLGDriver() {};
-    virtual ~MicroLLGDriver() {std::cout << "Killing LLG Driver\n";};
+    ~MicroLLGDriver() {std::cout << "Killing LLG Driver\n";};
 
+    // Testing constructor
+    MicroLLGDriver(MicroSim * sim, double * alpha, double gamma,
+                   double t, double dt) {
+        sim = sim;
+        alpha = alpha;
+        gamma = gamma;
+        t = t;
+        dt = dt;
+    }
+
+    // void compute_RHS(double * m, unsigned int n, double t);
+    //
+    void generate_RHS_function(double * alpha ...);
+
+    // TODO: declare all remaining values from the full form LLG eq
     double * alpha;
     double gamma;
     double t;
+    double dt;
     MicroSim * sim;
     Integrator * integrator;
 
     // Will get the parameters from a simulation class
-    void _setup(MicroSim * sim, double * alpha, double gamma, double t);
-    void add_integrator(Integrator * integrator);
+    void _setup(MicroSim * sim,
+                double * alpha, double gamma, 
+                // double * zl_jx, double * zl_jy, double * zl_jz, double zl_p, double zl_beta, double zl_u0,
+                // double * cpp_p, double * cpp_aJ, double * cpp_beta,
+                double t, double dt);
+    void add_integrator(IntegratorID integrator_id);
     void run_until(double t);
 };
 
@@ -25,8 +64,8 @@ public:
 // public:
 //     MicroLLGDriver() {};
 //     // ~MicroLLGDriver() {std::cout << "Killing base Driver\n";};
-// 
-// 
+//
+//
 // }
 
 // ----------------------------------------------------------------------------
@@ -35,30 +74,40 @@ public:
 class Integrator {
 public:
     Integrator() {};
+
+    // We probably should better use a constructor:
+    // Accepts N variables (e.g. 3 * n_spins) and a pointer to a function
     virtual void _setup(unsigned int N,
                         double dt,
-                        void (*f) (double * m, unsigned int len, double t),
-                        double * init_m) {};
+                        void (*f) (double * m, unsigned int n, double t)
+                        // double * init_m
+                        ) {};
     // Allow magnetisation to update (via LLG + effective field)
     // Here we store a pointer to the RHS calculation function set up by the
     // driver
-    void (*compute_RHS) (double * m, unsigned int len, double t) {};
-}
+    void (*compute_RHS) (double * m, unsigned int n, double t) {};
+    virtual void integration_step(double * m) {};
+
+    IntegratorID integrator_id;
+};
 
 // Integrators should be independent of any driver/sim class
 class Integrator_RK4: public Integrator {
 public:
     Integrator_RK4() {};
     virtual ~Integrator_RK4() {std::cout << "Killing RK4 integrator\n";};
+
     // Will get the parameters from a simulation class
     // void _setup(MicroSim * sim, Driver * driver);
     std::vector<double> rksteps;  // N len vector, we could also use an array
-    void integration_step(double * m);  // compute_RHS ??
-    // Accepts N variables (e.g. 3 * n_spins) and a pointer to a function
+
     void _setup(unsigned int N,
                 double dt,
-                void (*f) (double * m, unsigned int len, double t),
-                double * init_m);
+                void (*f) (double * m, unsigned int n, double t)
+                // double * init_m
+                );
+    void integration_step(double * m);
+
     unsigned int step_n;
     unsigned int N;
     double t;
--- native/src/m_driver.cpp
+++ native/src/m_driver.cpp
@@ -1,10 +1,32 @@
 #include "m_driver.h"
 
-
-void MicroLLGDriver::_setup(MicroSim * Sim) {
+void MicroLLGDriver::_setup(MicroSim * sim,
+                            double * alpha, double gamma, double t, double dt) {
 
     this->sim = sim;
+    this->alpha = alpha;
+    this->gamma = gamma;
+    this->t = t;
+    this->dt = dt;
 
+
+}
+// TODO: Set up LLG equation function: compute_RHS
+void MicroLLGDriver::compute_RHS(double * m, unsigned int n, double t) {
+
+}
+
+void MicroLLGDriver::add_integrator(IntegratorID integrator_id) {
+
+    switch(integrator_id) {
+        case RK4:
+            this->integrator = new Integrator_RK4();
+    }
+
+    this->integrator->_setup(this->sim->n, this->dt, 
+                             this->compute_RHS
+                             // this->
+                             );
 }
 
 // ----------------------------------------------------------------------------
@@ -12,27 +34,26 @@ void MicroLLGDriver::_setup(MicroSim * Sim) {
 void Integrator_RK4::_setup(unsigned int N,  // Change to unsigned long??
                             double dt,
                             // check these limits:
-                            void (*f) (double * m, unsigned int len, double t),
-                            double * init_m  // Might not be necessary!
+                            void (*f) (double * m, unsigned int n, double t)
+                            // double * init_m  // Might not be necessary!
                             ) {
     // Initial values
-    this->rk_steps(N * 4, 0.0);
+    this->rksteps.resize(N * 4, 0.0);
     this->step_n = 0;
     this->t = 0.0;
 
     this->dt = dt;
-    this->compute_RHS = &f;
+    this->compute_RHS = f;
 
-    for (unsigned int i = 0; i < N; ++i ) {
-        rk_steps[i] = init_m[i];
-    }
+    // for (unsigned int i = 0; i < N; ++i ) {
+    //     rksteps[i] = init_m[i];
+    // }
 }
 
 void Integrator_RK4::integration_step(double * m) {
 
-    this->rk_steps(N * 4, 0.0);
-    double t_factor[4] = {0.0, 0.5, 0.5, 1.0}
-    double m_factor[4] = {0.0, 0.5, 0.5, 1.0}
+    double t_factor[4] = {0.0, 0.5, 0.5, 1.0};
+    double m_factor[4] = {0.0, 0.5, 0.5, 1.0};
 
     for (unsigned int RKSTEP = 0; RKSTEP < 4; ++RKSTEP) {
 
@@ -42,24 +63,24 @@ void Integrator_RK4::integration_step(double * m) {
         for (unsigned int i = 0; i < this->N; ++i ) {
             unsigned int RKSTEP_idx = i + (RKSTEP) * this->N;
 
-            rk_steps[RKSTEP_idx] = m[i];
+            rksteps[RKSTEP_idx] = m[i];
 
             if (RKSTEP > 0) {
-                rk_steps[RKSTEP_idx] += m_factor[RKSTEP] * this->dt * rk_steps[i + (RKSTEP - 1) * this->N];
+                rksteps[RKSTEP_idx] += m_factor[RKSTEP] * this->dt * rksteps[i + (RKSTEP - 1) * this->N];
             }
 
         }
         // Update the corresponding values of the RK step vector
-        this->compute_RHS(&rk_steps[RKSTEP * N], N, t + t_factor[RKSTEP] * dt);
+        this->compute_RHS(&rksteps[RKSTEP * N], N, t + t_factor[RKSTEP] * dt);
     }
 
     // Update time and array
     this->t += this->dt;
     for (unsigned int i = 0; i < this->N; ++i) {
-        m[i] += (1 / 6) * this->dt * (    rk_steps[i] + 
-                                      2 * rk_steps[i + N] + 
-                                      2 * rk_steps[i + 2 * N] + 
-                                          rk_steps[i + 3 * N]);
+        m[i] += (1 / 6) * this->dt * (    rksteps[i] +
+                                      2 * rksteps[i + N] +
+                                      2 * rksteps[i + 2 * N] +
+                                          rksteps[i + 3 * N]);
     }
 
     // // RK4 step 1
@@ -69,7 +90,7 @@ void Integrator_RK4::integration_step(double * m) {
     // for (unsigned int i = 0; i < N; ++i ) {
     //     rk_steps[i + N] = m[i] + 0.5 * this->dt * rk_steps[i];
     // }
-    // this->compute_RHS(rk_steps.begin() + N, rk_steps.begin() + 2 * N, 
+    // this->compute_RHS(rk_steps.begin() + N, rk_steps.begin() + 2 * N,
     //                   rk_steps, t + 0.5 * dt);
 
     // // RK4 step 3
