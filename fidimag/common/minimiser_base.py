0ff3237bc94a4f242ef81faaa30add29677a8cef
davidcortesortuno <david.cortes.o@gmail.com>
2026-08-14 14:58:55 -0400

    Add filename arg to minimiser_base save_vtk() too

--- fidimag/common/minimiser_base.py
+++ fidimag/common/minimiser_base.py
@@ -99,11 +99,14 @@ class MinimiserBase(object):
     # -------------------------------------------------------------------------
     # SAVERS
 
-    def save_vtk(self):
+    def save_vtk(self, filename=None):
         """
         Save a VTK file with the magnetisation vector field and magnetic
         moments as cell data. Magnetic moments are saved in units of
         Bohr magnetons
+
+        If `filename` is given, the data is written to that single file
+        instead of the usual `{name}_{step:06d}` sequence.
 
         NOTE: It is recommended to use a *cell to point data* filter in
         Paraview or Mayavi to plot the vector field
@@ -115,7 +118,10 @@ class MinimiserBase(object):
                              name='magnetisation')
         self.VTK.save_vector(self.spin.reshape(-1, 3), name='spins')
 
-        self.VTK.write_file(step=self.step)
+        if filename is not None:
+            self.VTK.save_as(filename)
+        else:
+            self.VTK.write_file(step=self.step)
 
     def save_m(self, ZIP=False):
         """
